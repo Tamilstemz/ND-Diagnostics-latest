5e281cfef2943de5a4242989e202877a38b119c1
Tamilstemz <fsdev3@stemzglobal.com>
2025-06-14 20:02:33 +0530

    initial

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
+const repoName = "ND-Diagnostics-latest";
+
 export default defineConfig({
+  base: `/${repoName}/`,
   plugins: [
     react(),
     runtimeErrorOverlay(),
@@ -11,7 +14,7 @@ export default defineConfig({
     process.env.REPL_ID !== undefined
       ? [
           await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
+            m.cartographer()
           ),
         ]
       : []),
@@ -25,7 +28,7 @@ export default defineConfig({
   },
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    outDir: path.resolve(import.meta.dirname, "dist"),
     emptyOutDir: true,
   },
   server: {
